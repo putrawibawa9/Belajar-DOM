b6c0d9cf1ba84bc87fff305128ccc58c0f1fb450
I Gede Putra Wibawa <putrawibawa7@gmail.com>
2024-02-05 14:28:04 +0800

    dom traversal part 1

--- dom traversal/part1.js
+++ dom traversal/part1.js
@@ -1,7 +1,17 @@
 closeButton = document.querySelectorAll(".close");
 
-for (let i = 0; i < closeButton.length; i++) {
-  closeButton[i].addEventListener("click", function () {
-    alert([i]);
+// for (let i = 0; i < closeButton.length; i++) {
+//   closeButton[i].addEventListener("click", function (x) {
+//     x.target.parentElement.remove();
+//   });
+// }
+
+closeButton.forEach(function (i) {
+  i.addEventListener("click", function (z) {
+    z.target.parentElement.style.display = "none";
   });
-}
+});
+
+nama = document.querySelector(".nama");
+
+console.log(nama.nextElementSibling);
